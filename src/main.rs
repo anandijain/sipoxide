01cc59bfa3e20e45a05478fe178292a16ea57c33
anandijain <anandj@uchicago.edu>
2020-03-30 21:01:37 -0500

    added routes

--- src/main.rs
+++ src/main.rs
@@ -122,22 +122,16 @@ fn lines_to_csv(lines: Vec<lines::Root>, write_fn: String) -> Result<(), Box<dyn
 #[tokio::main]
 async fn lines() -> Result<Vec<lines::Root>, reqwest::Error> {
     let lines_url = "https://www.bovada.lv/services/sports/event/v2/events/A/description/";
-    let lines_fn = "./data/root.csv";
     let res = reqwest::get(lines_url).await?;
     println!("{} status: {}", lines_url, res.status());
     let body = res.text().await?;
     let lines: Vec<lines::Root> = serde_json::from_str(&body.to_string()).unwrap();
-    // if let Err(err) = lines_to_csv(lines, lines_fn.to_string()) {
-    //     println!("{}", err);
-    // }
-
      Ok(lines)
 }
 
 #[tokio::main]
 async fn scores() -> Result<Vec<scores::Root>, reqwest::Error> {
     let scores_url = "https://services.bovada.lv/services/sports/results/api/v1/scores/";
-    let scores_fn = "./data/scores.csv";
     let res = reqwest::get(scores_url).await?;
     println!("{} status: {}", scores_url, res.status());
     let body = res.text().await?;
@@ -146,11 +140,15 @@ async fn scores() -> Result<Vec<scores::Root>, reqwest::Error> {
 }
 
 fn main() {
-    let l = lines();
+    // let lines_fn = "./data/root.csv";
+    // let scores_fn = "./data/scores.csv";
+
+    // let l = lines();
     // let s = scores();
-    // for elt in l.iter() {
-    //     println!("{:#?}", elt)
+        // if let Err(err) = lines_to_csv(lines, lines_fn.to_string()) {
+    //     println!("{}", err);
     // }
+
     rkt_main()
 }
 
@@ -161,34 +159,43 @@ fn index () -> &'static str {
 
 #[get("/scores")]
 fn route_scores() -> Json<Vec<scores::Root>>  {
-    // let fn = "./data/scores.csv";
-    // let file = File::open(fn)?;
-    // let mut rdr = csv::Reader::from_reader(file);
     let s = scores(); 
-    for elt in &s {
-        println!("{:?}", elt);
-    }
     match s {
         Ok(s) => Json(s),
         _ => Json(vec![])
     }
 }
 
+#[get("/scorescsv")]
+fn route_scores_csv() -> String {
+    let d = fs::read_to_string("./data/scores.csv").expect("error in reading to string");
+    return d;
+}
+
+
 #[get("/lines")]
 fn route_lines() -> Json<Vec<lines::Root>>  {
-    // let fn = "./data/scores.csv";
-    // let file = File::open(fn)?;
-    // let mut rdr = csv::Reader::from_reader(file);
+    
     let s = lines(); 
-    // for elt in &s {
-    //     println!("{:?}", elt);
-    // }
     match s {
         Ok(s) => Json(s),
         _ => Json(vec![])
     }
 }
+
+#[get("/linescsv")]
+fn route_lines_csv() -> String {
+    let d = fs::read_to_string("./data/lines.csv").expect("error in reading to string");
+    return d;
+}
+
+#[get("/joined_csv")]
+fn route_joined_csv() -> String {
+    let d = fs::read_to_string("./data/bov_joined.csv").expect("error in reading to string");
+    return d;
+}
+
 fn rkt_main() {
-    rocket::ignite().mount("/", routes![index, route_scores, route_lines]).launch();
+    rocket::ignite().mount("/", routes![index, route_scores, route_scores_csv, route_lines, route_lines_csv, route_joined_csv]).launch();
 }
 
